61c80a23219d0dfafeba9c083bf84d2cec17b3e3
Yot-el <yvsuteots@gmail.com>
2023-07-21 16:10:24 +0200

    Исправление ошибок

--- js/utils/util.js
+++ js/utils/util.js
@@ -2,13 +2,13 @@ const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min) + 
 
 const isEscapeKey = (event) => event.key === 'Escape';
 
-function debounce (callback, timeoutDelay) {
+const debounce = (callback, timeoutDelay) => {
   let timeoutId;
 
   return (...rest) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
-}
+};
 
 export { getRandomNumber, isEscapeKey, debounce };
